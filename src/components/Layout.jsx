0dbbac16854817b5c4d4653ee2dd19edc0572774
Lakshmi Sowmya .L <sowmyasri4002@gmail.com>
2024-08-11 15:38:02 +0530

    msg

---
+++ src/components/Layout.jsx
@@ -0,0 +1,12 @@
+import Note from "./Note";
+
+const Layout = () =>{
+return (
+    <div>
+        <h1>hii</h1>
+        <Note/>
+    </div>
+    );
+};
+
+export default Layout;
---
+++ src/components/Note.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import "./Note.css";
+const Note = () => {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [vis,setVis]= useState(true);
+
+  const handleAddNote = (e) => {
+    e.preventDefault();
+    const newNote = { title, content };
+    setNotes([...notes, newNote]);
+    setTitle("");
+    setContent("");
+  };
+
+  const handleDeleteNote = (index) => {
+    const newNotes = notes.filter((_, i) => i !== index);
+    setNotes(newNotes);
+  };
+
+  return (
+    <div className="app-container">
+      <form className="note-form" onSubmit={handleAddNote}>
+        <input
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <textarea
+          placeholder="Content"
+          rows={10}
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          required
+        />
+        <button type="submit">Add Note</button>
+      </form>
+
+      <div className="notes-grid">
+        {notes.map((note, index) => (
+          <div
+            className="note-item"
+            key={index}
+          >
+            <div className="notes-header">
+              <button onClick={() => handleDeleteNote(index)}>x</button>
+            </div>
+            <div className="note-content">
+              <h2>{note.title}</h2>
+              <p>{note.content}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Note;
