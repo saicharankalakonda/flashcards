d82e52c792f494012b4cc7b186d86e2a6b44eda7
saicharankalakonda <ksaicharan29@gmail.com>
2024-08-12 09:22:01 +0530

    flashcard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import Layout from "./components/Layout.jsx";
-
+import FlipCard from "./components/FlipCard.jsx";
+import Carousel from "./components/Carousel.jsx";
 function App() {
+
+  const notes = [
+    {title:"question1",content:"answer1"},
+    {title:"question2",content:"answer2"},
+    {title:"question3",content:"answer3"},
+    {title:"question4",content:"answer4"},
+    {title:"question5",content:"answer5"},
+    {title:"question6",content:"answer6"},
+    {title:"question7",content:"answer7"}
+  ]
   return (
     <div className="App">
-      <Layout />
+        <Carousel notes ={notes} />
     </div>
   );
 }
